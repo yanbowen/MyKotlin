ec0b39ad3d6a293ec52d52185063fab5eb444fab
闫博文 <yanbowen90@outlook.com>
2017-06-20 20:49:56 +0800

    Initial commit

---
+++ src/Test.kt
@@ -0,0 +1,6 @@
+/**
+ * Created by yanbowen on 6/20/2017.
+ */
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
---
+++ src/maxFunction.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by yanbowen on 6/20/2017.
+ */
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+
+fun main(args: Array<String>) {
+    println(max(1, 2))
+}
